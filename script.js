568e114a7ba677edd7fc9ee27a1cc34298b37c12
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2026-05-26 19:27:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 const container = document.querySelector('.items');
 
 let isDown = false;
-let startX;
-let scrollLeft;
+let startX = 0;
+let scrollLeft = 0;
 
 container.addEventListener('mousedown', (e) => {
   isDown = true;
   container.classList.add('active');
 
-  startX = e.pageX - container.offsetLeft;
+  startX = e.pageX;
   scrollLeft = container.scrollLeft;
-});
-
-container.addEventListener('mouseleave', () => {
-  isDown = false;
-  container.classList.remove('active');
 });
 
 container.addEventListener('mouseup', () => {
@@ -22,13 +17,15 @@ container.addEventListener('mouseup', () => {
   container.classList.remove('active');
 });
 
+container.addEventListener('mouseleave', () => {
+  isDown = false;
+  container.classList.remove('active');
+});
+
 container.addEventListener('mousemove', (e) => {
   if (!isDown) return;
 
-  e.preventDefault();
+  const walk = startX - e.pageX;
 
-  const x = e.pageX - container.offsetLeft;
-  const walk = (x - startX) * 2;
-
-  container.scrollLeft = scrollLeft - walk;
+  container.scrollLeft = scrollLeft + walk;
 });
